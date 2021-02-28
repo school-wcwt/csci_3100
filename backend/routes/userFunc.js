d333ce41bee573c25850776b272bddf749ffe440
Jon <jonathan.lph@hotmail.com>
2021-02-28 22:05:03 +0800

    Changes to userFunc.js.

--- backend/routes/userFunc.js
+++ backend/routes/userFunc.js
@@ -8,51 +8,39 @@ var bcrypt = require('bcrypt');
 
 var auth = (filter, data) => {
     return new Promise((resolve, reject) => {
-        Entity
-        .findOne(filter)
-        .exec((err, entity) => {
-            if (err) return reject(err);
-            if (entity.type || entity == null) return reject('Entity not found.');
-            bcrypt.compare(entity.password, data.password, (err, res) => {
-                if (err) return reject(err);
-                if (!res) return reject('Incorrect password.')
-                findEntity({entityID: entityID})
-                .then(loginedEntity => {
-                    return resolve(loginedEntity)
-                })
-            })
-        })
+        (async () => { try {
+            var entity = await Entity.findOne(filter).exec()
+            if (entity.type || entity == null) throw new Error('Entity not found.');
+            var match = await bcrypt.compare(entity.password, data.password) 
+            if (!match) throw new Error('Incorrect password.')
+            var loginedEntity = await findEntity({entityID: entityID})
+            return resolve(loginedEntity);
+        } catch(err) { return reject(err) } })();
     })
 }
 
 var updateFollow = (authorFilter, targetFilter, addFlag = 1) => {
     return new Promise((resolve, reject) => {
-        findEntityID(authorFilter)
-        .then(author => {
-            if (author == null) throw new Error('Entity not found.');
-            findEntityID(targetFilter)
-            .then(target => {
-                if (target == null) throw new Error('Entity not found.');
-                var followType = target.type ? 'followingRest' : 'followingUser';
-                var operation = addFlag ? '$push' : '$pull';
-                var targetQuery = { [operation]: {"followed":   author.entity_id} };
-                var authorQuery = { [operation]: {[followType]: target.entity_id} };
-                Entity.findOneAndUpdate({entityID: target.entityID}, targetQuery)
-                .exec(err => { if (err) throw err; })
-                User.findOneAndUpdate({entityID: author.entityID}, authorQuery)
-                .exec((err, oldUser) => {
-                    if (err) throw err;
-                    findEntity(
-                        {entityID: oldUser.entityID}, 0, 
-                        {subentityPop: {path: followType, select: 'entityID'}}
-                    )
-                    .then(updatedEntity => { return resolve(updatedEntity) })
-                    .catch(err => { throw err });
-                })
-            })
-            .catch(err => { throw err })
-        })
-        .catch(err => { return reject(err) });
+        (async () => { try { 
+            var [author, target] = await Promise.all([
+                findEntityID(authorFilter), 
+                findEntityID(targetFilter)
+            ]);
+            if (author == null || target == null) throw new Error('Entity not found.');
+            var followType  = target.type ? 'followingRest' : 'followingUser';
+            var operation   = addFlag ? '$push' : '$pull';
+            var targetQuery = { [operation]: {"followed":   author.entity_id} };
+            var authorQuery = { [operation]: {[followType]: target.entity_id} };
+            var [oldEntity, oldUser] = await Promise.all([
+                Entity.findOneAndUpdate({entityID: target.entityID}, targetQuery).exec(),
+                User.findOneAndUpdate({entityID: author.entityID}, authorQuery).exec()
+            ])
+            var updatedEntity = await findEntity(
+                {entityID: oldUser.entityID}, 0, 
+                {subentityPop: {path: followType, select: 'entityID'}}
+            )
+            return resolve(updatedEntity);
+        } catch(err) { return reject(err) } })();
     })
 }
 
@@ -97,38 +85,27 @@ var updateList = (authorFilter, listName, addFlag = 1) => {
 
 var updateListContent = (authorFilter, targetFilter, listName, addFlag = 1) => {
     return new Promise((resolve, reject) => {
-        findEntityID(authorFilter)
-        .then(author => {
-            if (author == null) throw new Error('Entity not found.');
-            findEntityID(targetFilter)
-            .then(target => {
-                if (target == null) throw new Error('Entity not found.');
-                var operation = addFlag ? '$push' : '$pull';
-                User.findOneAndUpdate(
-                    {entityID: author.entityID},
-                    {[operation]: {'groupList.$[elem].content': target.entity_id}},
-                    {arrayFilters: [{'elem.name': listName}]},
-                ).exec((err, oldUser) => {
-                    if (err) throw err;
-                    findEntity(
-                        {entityID: oldUser.entityID}, 0,
-                        {subentityPop: {path: groupList, select: 'entityID'}}
-                    )
-                    .then(updatedEntity => { return resolve(updatedEntity) })
-                    .catch(err => { throw err });
-                })
-            })
-            .catch(err => { throw err })
-        })
-        .catch(err => { return reject(err) });
+        (async () => { try { 
+            var [author, target] = await Promise.all([
+                findEntityID(authorFilter), 
+                findEntityID(targetFilter),
+            ]);
+            if (author == null || target == null) throw new Error('Entity not found.');
+            var operation = addFlag ? '$push' : '$pull';
+            var oldGroupList = await GroupList.findOneAndUpdate(
+                {author: author.entity_id, name: listName},
+                {[operation]: {'content': target.entity_id}}
+            ).exec();
+            var updatedEntity = await findEntity(
+                {entityID: author.entityID}, 0,
+                {subentityPop: {path: 'groupList', select: 'name content', 
+                    populate:  {path: 'content', select: 'entityID name profPhoto'}}}
+            )
+            return resolve(updatedEntity)
+        } catch(err) { return reject(err) } })();
     })
 }
 
-
-/*
-                    {subentityPop: {path: 'groupList', select: 'name content', 
-                        populate: {path: 'content', select: 'entityID name profPhoto'}}
-                        */
 module.exports = {
     auth,
     updateFollow,
