a803bf96423d59566d8b8713cff7b60a66caa4ef
wcwt <1155109240@link.cuhk.edu.hk>
2021-03-09 01:27:13 +0800

    updated email testing case

--- my-app/src/pages/main/index.js
+++ my-app/src/pages/main/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PrimarySearchAppBar from './component/appBar/appBar.js';
 //import TopHead from './component/topHead/topHead.js';
-
+import Form from '../../component/email/Form';
 const Main = ()=>{
     return (
         <div>
             <PrimarySearchAppBar/>
+            <Form/>
         </div>        
     )
 }
