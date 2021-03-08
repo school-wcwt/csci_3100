180df7407c976f5b33792cba5a78f577cc679dca
wcwt <1155109240@link.cuhk.edu.hk>
2021-03-08 22:40:43 +0800

    final upadte on login page

--- backend/models/Salt.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var SaltSchema = mongoose.Schema({
-    entityID:  { type: String, required: true, unique: true },
-    username:  { type: String, required: true, unique: true }, // Rest: name without space
-});s
-
-module.exports = mongoose.model('Salt',  SaltSchema);
