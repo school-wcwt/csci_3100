93ade03b6823a03cf2d82a0d06d1271b860ce3b4
wcwt <1155109240@link.cuhk.edu.hk>
2021-03-15 22:54:19 +0800

    update

--- my-app/src/pages/main/index.js
+++ my-app/src/pages/main/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import PrimarySearchAppBar from './component/appBar/appBar.js';
 //import TopHead from './component/topHead/topHead.js';
+
+const part1 = () => {
+    return (
+        <html>
+
+        </html>
+    );
+}
+
+const part2 = () => {
+    return (
+        <html>
+
+        </html>
+    );
+}
+
+const test = () => {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
 const Main = ()=>{
     return (
         <div>
-            <PrimarySearchAppBar/>
+            <part1/> 
+            <part2/>
+            <test/>     
         </div>        
     )
 }
