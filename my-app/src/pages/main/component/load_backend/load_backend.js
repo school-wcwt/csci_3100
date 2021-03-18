ee8a89301e54794e4e34c253663bda57fbda6cd3
Pong <garyao12@gmail.com>
2021-03-18 19:11:06 +0800

    axios for getting entity

--- my-app/src/pages/main/component/load_backend/load_backend.js
+++ my-app/src/pages/main/component/load_backend/load_backend.js
@@ -2,22 +2,35 @@ import React from "react";
 import axios from '../../../../axiosConfig';
 import { BrowserRouter as Router, Switch, Redirect,Route, Link } from 'react-router-dom';
 
-const get_entity =(entityID) => {
-    axios({
+
+//Assume I get authorID already
+ function get_entity(entityID){
+    console.log('hey');
+    return axios({
         method: 'GET',
         url: '/entity/'+entityID,
         withCredentials: false,
     })
-    .then(res => {
-        console.log("sent");
-        console.log(res);
-        <Redirect to="/" />
-        return res;
+    .then ( res =>{
+        var send=   res.data;
+       // console.log(res)
+        return send;
     })
     .catch(err => {
         console.log(err.message);
-        return ('error');
-    })
-};
+        console.log('error');
+    }
+    )
+}
 
-export {get_entity};
+/* to call the above function, add the following:
+    var authorID='joe-9363';
+    var author=  get_entity(authorID);
+    get_entity(authorID).then(data=>{
+        console.log('1 inside');
+        console.log(data);
+        console.log('2 inside')
+        return data;
+    });
+*/
+export default get_entity;
