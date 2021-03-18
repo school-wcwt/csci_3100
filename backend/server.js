d1f05375cc36248af41201923bb0201b2b6aeb40
jonathan-lph <hhkk0720@hotmail.com>
2021-03-19 00:24:02 +0800

    Removed test cases.

--- backend/server.js
+++ backend/server.js
@@ -21,17 +21,4 @@ mongoose.connect('mongodb://'+username+':'+password+'@localhost/csci3100');
 app.use('/entity', require('./routes/entity'));
 app.use('/user', require('./routes/user'));
 
-var entityFunc = require('./routes/entityFunc');
-var userFunc = require('./routes/userFunc');
-const postFunc = require('./routes/postFunc');
-const tagFunc = require('./routes/hashtagFunc');
-const commentFunc = require('./routes/commentFunc');
-
 app.listen(3100);
-
-//jon-0571         60533f9e6019086c692576a9
-//jon-rest2-6053   60533f7de94b6f6bc1a40599
-
-/*postFunc.createPost({author: '60533f9e6019086c692576a9', target: '60533f7de94b6f6bc1a40599'}, 'jon-0571', {
-    type: 1, content: 'Fuck me', hashtag: ['Japan', 'OK-la']
-}).then(res => console.log(res));*/
