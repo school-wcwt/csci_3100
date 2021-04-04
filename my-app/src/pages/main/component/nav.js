89a3cf8625a5cb342240c5e6b253b0353eb1d7a5
kf <kf@csci.us-central1-a.c.even-sun-308505.internal>
2021-04-04 10:48:20 +0000

    nav.js update

--- my-app/src/pages/main/component/nav.js
+++ my-app/src/pages/main/component/nav.js
@@ -1,21 +1,22 @@
 import {Navbar,Form,Button,FormControl,Nav} from 'react-bootstrap';
 import logo from '../../../image/icon2.PNG';
+import {Link} from 'react-router-dom';
 import {GetUserObj} from '../../services/authService';
 
 function NAVbar(){
     const entitiesID = GetUserObj.entitiesID;
     return(
 <>
-  <Navbar collapseOnSelect expand="md" bg="dark" fixed="top">
+  <Navbar collapseOnSelect expand="lg" bg="dark" fixed="top" variant="dark">
     <Navbar.Brand href="#home"><img src={logo} height="50" width="150" alt="mATE." className=" float-left" id="icon"/></Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="text-center mx-auto">
-    <Nav.Link href="/main" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Home</Nav.Link>
-    <Nav.Link href="/dsicover" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Dicover</Nav.Link>
-    <Nav.Link href="#create" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Create Post</Nav.Link>
-    <Nav.Link href="/restregister" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Creata Restaurant</Nav.Link>
-    <Nav.Link href="/userprofile/{entitiesID}" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>My Account</Nav.Link>
+    <Nav.Link as={Link} to="/main" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Home</Nav.Link>
+    <Nav.Link as={Link} to="/discover" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Discover</Nav.Link>
+    <Nav.Link as={Link} to="/create" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Create Post</Nav.Link>
+    <Nav.Link as={Link} to="/restregister" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>Create Restaurant</Nav.Link>
+    <Nav.Link as={Link} to="/userprofile/{entitiesID}" className="mr-xl-5 mr-md-4" style={{color: "LightCoral", fontWeight:800}}>My Account</Nav.Link>
     </Nav>
     <Form inline>
       <div className="mx-auto row">
