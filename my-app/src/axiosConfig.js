1464124a52e23def7a8d22ccea61e73e6910cbe3
jonathan-lph <hhkk0720@hotmail.com>
2021-04-07 11:56:34 +0000

    Port config.

--- my-app/src/axiosConfig.js
+++ my-app/src/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const Mongo_port = require('./port'); // if change, also need to change in ../backend/server.js's app.listen(3100);
+const backendPort = process.env.REACT_APP_BACKEND_PORT; // if change, also need to change in ../backend/server.js's app.listen(3100);
 
 const instance = axios.create({
-    baseURL: `http://localhost:${Mongo_port}/`,
+    baseURL: `http://localhost:${backendPort}/`,
     withCredentials: true,
 });
 
