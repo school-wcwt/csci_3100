8de67c798ab5b4fa51719114f122f05edfdbda70
school-wcwt <1155109240@link.cuhk.edu.hk>
2021-04-11 09:58:28 +0000

    upatded

--- my-app/src/pages/main/index.js
+++ my-app/src/pages/main/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import NAVbar from "./component/nav.js";
-import state from "../userState";
-import  {Auth} from '../services/authService';
-import { Typography, Box, CssBaseline } from '@material-ui/core';
-
-=======
 import NavBar from "./component/nav";
 import { Post } from "../../component/post/post.js";
 import { Auth } from '../services/authService';
 import { CssBaseline } from '@material-ui/core';
->>>>>>> 7c3144f4dd611147b144c9289a294adb2b696550
 
 const Main = () => {
     Auth();
